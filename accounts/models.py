9ab7b59f23ffd8298b63d86ea9bfc8eecb28a8dd
jyothish <jyothishckchandran@gmail.com>
2025-10-28 12:30:09 +0530

    FEAT: Added default avatar to Profile model and static files to prevent 404 errors on lost media.

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,12 @@ class Profile(models.Model):
     location = models.CharField(max_length=100, blank=True)
     bio = models.TextField(blank=True)
   
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True) 
+    avatar = models.ImageField(
+        upload_to='avatars/', 
+        default='accounts/default_profile.png',  
+        blank=True, 
+        null=True
+    )
 
     def __str__(self):
         return f"{self.user.username}'s profile"
